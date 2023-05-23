bbdb640702318cca56068aca6303097258ba6f13
Paulina Gonzalez <paulinagzzc@gmail.com>
2023-05-22 18:42:14 -0600

    Add articles listing feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :index]
 end
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
